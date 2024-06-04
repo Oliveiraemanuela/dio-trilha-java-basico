0a679988eb6ed5ef53e5fa02d6d9d2c870fd9220
emanuela <oliveiraemanuela1225@gmail.com>
2024-06-04 14:48:20 -0300

    novas alterações

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,3 +1,18 @@
 public class Operadores {
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        //x repetição
+
+        numero++;
+
+        System.out.println(numero);
+
+
+
+    }
+    
+    
 
 }
