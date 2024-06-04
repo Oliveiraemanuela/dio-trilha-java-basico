e2008bf282db0ffc84f9bf73dceaeadc1a8e862b
emanuela <oliveiraemanuela1225@gmail.com>
2024-06-04 16:01:32 -0300

    atrinuicão

--- tipos-variaveis/src/OperadoresTernarios.java
+++ tipos-variaveis/src/OperadoresTernarios.java
@@ -1,3 +1,14 @@
 public class OperadoresTernarios {
+    public static void main(String[] args) {
+        int a, b;
 
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        //int resultado = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+
+    }
 }
