b01b48b2da8dea5fd1f2a87c5f7b843e09919206
emanuela <oliveiraemanuela1225@gmail.com>
2024-06-04 14:55:15 -0300

    alteracoes

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,15 +1,39 @@
 public class Operadores {
     public static void main(String[] args) {
-        
+        String nomeCompleto = "Emanuela " + "Sabino";
+
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+"1"+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1+1+1);
+
+        System.out.println(concatenacao);
+
         int numero = 5;
 
-        //x repetição
-
-        numero++;
+        numero = - numero;
 
         System.out.println(numero);
 
+        numero = - numero;
 
+        System.out.println(numero);
+        
 
     }
     
--- tipos-variaveis/src/OperadoresUnarios.java
+++ tipos-variaveis/src/OperadoresUnarios.java
@@ -1,3 +1,14 @@
 public class OperadoresUnarios {
 
+    public static void main(String[] args) {
+        int numero = 5;
+
+    //x repetição
+
+    numero++;
+
+    System.out.println(numero);
+        
+    }
+    
 }
